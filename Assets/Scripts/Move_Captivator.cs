3c73634703a66e14f387ce750350b72702257c69
Dadalifeee <mat.lunardonjr@gmail.com>
2019-05-01 15:48:28 +0200

    aloooo

---
+++ Assets/Scripts/Move_Captivator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move_Captivator : MonoBehaviour
+{
+    private Vector3 Target;
+    private bool Traveling;
+    [SerializeField]
+    Transform Destination1;
+    NavMeshAgent Captivator;
+
+    [SerializeField]
+    Transform Destination2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Captivator = this.GetComponent<NavMeshAgent>();
+        StartCoroutine(SetDestination());
+    }
+
+    IEnumerator SetDestination()
+    {
+        Traveling = false;
+        while (true)
+        {
+            Target = Destination2.transform.position;
+            Captivator.SetDestination(Target);
+            yield return new WaitForSeconds(3);
+            Target = Destination1.transform.position;
+            Captivator.SetDestination(Target);
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
---
+++ Assets/Scripts/Move_Spikes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Spikes : MonoBehaviour
+{
+    public Rigidbody Spikes;
+    private Vector3 Movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spikes = GetComponent<Rigidbody>();
+        StartCoroutine(Move());
+        Movement = new Vector3();
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            Movement = new Vector3(transform.position.x, (transform.position.y - 0.5f), transform.position.z);
+            Spikes.MovePosition(Movement);
+            yield return new WaitForSeconds(3);
+            Movement = new Vector3(transform.position.x, (transform.position.y + 0.5f), transform.position.z);
+            Spikes.MovePosition(Movement);
+        }
+    }
+}
